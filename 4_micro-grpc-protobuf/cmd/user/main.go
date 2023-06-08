4667810533ed1a6be61925ec0294a7cb17e7d629
zhuyasen <g.zhufuyi@gmail.com>
2023-06-08 21:56:10 +0800

    user rpc code base on protobuf

---
+++ 4_micro-grpc-protobuf/cmd/user/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"user/cmd/user/initial"
+
+	"github.com/zhufuyi/sponge/pkg/app"
+)
+
+func main() {
+	initial.Config()
+	servers := initial.RegisterServers()
+	closes := initial.RegisterClose(servers)
+
+	a := app.New(servers, closes)
+	a.Run()
+}
---
+++ 4_micro-grpc-protobuf/internal/config/user_test.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	"testing"
+
+	"user/configs"
+
+	"github.com/zhufuyi/sponge/pkg/gofile"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInit(t *testing.T) {
+	configFile := configs.Path("user.yml")
+	err := Init(configFile)
+	if gofile.IsExists(configFile) {
+		assert.NoError(t, err)
+	} else {
+		assert.Error(t, err)
+	}
+
+	c := Get()
+	assert.NotNil(t, c)
+
+	str := Show()
+	assert.NotEmpty(t, str)
+
+	// set nil
+	Set(nil)
+	defer func() {
+		recover()
+	}()
+	Get()
+}
+
+func TestInitNacos(t *testing.T) {
+	configFile := configs.Path("user_cc.yml")
+	_, err := NewCenter(configFile)
+	if gofile.IsExists(configFile) {
+		assert.NoError(t, err)
+	} else {
+		assert.Error(t, err)
+	}
+}
